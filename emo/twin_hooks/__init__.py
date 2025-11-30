638ec9c7c49d087e655d278dc89a076d9bcf27c8
avradri <avram3scu@outlook.com>
2025-11-30 20:57:32 +0100

    chore(twin-hooks): expose DestinE and climate ensemble hooks.

--- emo/twin_hooks/__init__.py
+++ emo/twin_hooks/__init__.py
@@ -1,6 +1,26 @@
-"""
-Hooks for Earth-system and climate-model digital twins.
+# emo/twin_hooks/__init__.py
+from .destine import (
+    DestineClient,
+    DestineConfig,
+    DestineCollectionSummary,
+    DestineItemSummary,
+    summarise_variable_statistics,
+    build_emo_destine_overlay,
+)
+from .climate_ensembles import (
+    ClimateEnsembleMember,
+    ensemble_from_dataframe,
+    prepare_ensemble_for_information_geometry,
+)
 
-- Destination Earth (DestinE)
-- Climate-model ensembles (CMIP, ECMWF, NOAA)
-"""
+__all__ = [
+    "DestineClient",
+    "DestineConfig",
+    "DestineCollectionSummary",
+    "DestineItemSummary",
+    "summarise_variable_statistics",
+    "build_emo_destine_overlay",
+    "ClimateEnsembleMember",
+    "ensemble_from_dataframe",
+    "prepare_ensemble_for_information_geometry",
+]
