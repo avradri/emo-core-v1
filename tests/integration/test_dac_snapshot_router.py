11422fca341ec5e14e10899297338caf61d7895c
Adrian N Avramescu <avram3scu@outlook.com>
2026-04-15 16:58:15 +0200

    Use computed DAC flow in snapshot route test

--- tests/integration/test_dac_snapshot_router.py
+++ tests/integration/test_dac_snapshot_router.py
@@ -16,8 +16,8 @@ def test_get_dac_snapshot():
         "metrics": {
             "warning_to_policy_lag_days": 3,
             "warning_to_delivery_lag_days": 5,
-            "alert_to_policy_conversion_rate": 0.4,
-            "implementation_persistence_30d": 0.7,
-            "declared_vs_funded_gap": 0.2,
+            "alert_to_policy_conversion_rate": 1.0,
+            "implementation_persistence_30d": 1.0,
+            "declared_vs_funded_gap": 0.0,
         },
     }
