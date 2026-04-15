633fd9cd0bf501102d2724717e3a3a7380ecf1ff
Adrian N Avramescu <avram3scu@outlook.com>
2026-04-15 17:22:05 +0200

    Use shared compare metrics service in DAC compare service

--- api/services/dac_compare_service.py
+++ api/services/dac_compare_service.py
@@ -1,30 +1,13 @@
 from api.schemas.dac_compare import DACCompareResponse, DACCompareSide
-from api.services.dac_compare_demo_flow_service import get_disaster_compare_demo_flows
-from emo.metrics.dac.contradiction import declared_vs_funded_gap
-from emo.metrics.dac.lag import warning_to_policy_lag_days
+from api.services.dac_compare_shared_metrics_service import (
+    get_disaster_compare_demo_metrics,
+)
 
 
 def get_dac_compare() -> DACCompareResponse:
-    flows = get_disaster_compare_demo_flows()
-
-    left = flows["left"]
-    right = flows["right"]
+    metrics = get_disaster_compare_demo_metrics()
 
     return DACCompareResponse(
-        left=DACCompareSide(
-            jurisdiction=left["policy"].jurisdiction,
-            warning_to_policy_lag_days=warning_to_policy_lag_days(
-                left["diagnostic"],
-                left["policy"],
-            ),
-            declared_vs_funded_gap=declared_vs_funded_gap(1, 1),
-        ),
-        right=DACCompareSide(
-            jurisdiction=right["policy"].jurisdiction,
-            warning_to_policy_lag_days=warning_to_policy_lag_days(
-                right["diagnostic"],
-                right["policy"],
-            ),
-            declared_vs_funded_gap=declared_vs_funded_gap(1, 1),
-        ),
+        left=DACCompareSide(**metrics["left"]),
+        right=DACCompareSide(**metrics["right"]),
     )
